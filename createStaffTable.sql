b29b102954eebdfbc630121f3cc68fc2d65a6c9e
Duy Tran <drytuna@gmail.com>
2015-03-20 13:28:44 -0400

    all 4 buttons are functional
    All 4 buttons are working correctly with the database
    Changed scope to Session
    Need to work on validation and error handling

--- createStaffTable.sql
+++ createStaffTable.sql
@@ -2,6 +2,8 @@
 drop database Week7;
 create database Week7;
 */
+
+use Week7;
 drop table if exists Staff;
 create table Staff (
 	id char(9) not null,
@@ -18,13 +20,14 @@ create table Staff (
 
 insert into Staff values (
 '111', 'Doji', '', 'Shuten', '12345 Ayakashi Dr.', 'Tokyo', 'GG',
-'987-654-0123', 'shutendoji@ayakashi.gg');
+'9876540123', 'shutendoji@ayakashi.gg');
 insert into Staff values (
 '222', 'Doji', '', 'Giri', '23456 Ayakashi Dr.', 'Tokyo', 'GG',
-'654-789-0123', 'dojigiri@ayakashi.gg');
+'6547890123', 'dojigiri@ayakashi.gg');
 insert into Staff values (
 '333', 'Takeda', '', 'Shingen', '34567 Ayakashi Dr.', 'Tokyo', 'GG',
-'123-654-0987', 'shingentakeda@ayakashi.gg');
+'1236540987', 'shingentakeda@ayakashi.gg');
 insert into Staff values (
 '444', 'Hime', '', 'Kushinada', '45678 Ayakashi Dr.', 'Tokyo', 'GG',
-'321-456-8790', 'kushinadahime@ayakashi.gg');
+'3214568790', 'kushinadahime@ayakashi.gg');
+
--- src/resources/StaffRegistrationBean.java
+++ src/resources/StaffRegistrationBean.java
@@ -6,7 +6,6 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.context.RequestScoped;
@@ -14,7 +13,7 @@ import javax.enterprise.context.SessionScoped;
 import javax.inject.Named;
 
 @Named(value = "staff")
-@RequestScoped
+@SessionScoped
 public class StaffRegistrationBean implements Serializable{
 	private String id;
 	private String lastName;
@@ -28,8 +27,7 @@ public class StaffRegistrationBean implements Serializable{
 
 	private String status = "Nothing stored";
 	
-	private Connection conn;
-	private PreparedStatement pstmt;
+	private PreparedStatement view, insert, update;
 	
 	public StaffRegistrationBean() {
 		initializeJdbc();
@@ -43,55 +41,132 @@ public class StaffRegistrationBean implements Serializable{
 			System.out.println("Driver loaded");
 
 			// Establish a connection
-			conn = DriverManager.getConnection(
+			Connection conn = DriverManager.getConnection(
 					"jdbc:mysql://localhost:3306/Week7", "drytuna", "Pa$$word");
+			status = "Database Connected.";
 
-			// Create a Statement
-			pstmt = conn.prepareStatement("insert into Staff (id, lastName,"
+			// Create Statements
+			view = conn.prepareStatement("select * from Staff where id = ?");
+			
+			insert = conn.prepareStatement("insert into Staff (id, lastName,"
 					+ " mi, firstName, address, city, state,"
 					+ " telephone, email) values (?, ?, ?, ?, ?, ?, ?, ?, ?)");
+			
+			update = conn.prepareStatement("update Staff set "
+					+ " lastName = ?, mi = ?, firstName = ?,"
+					+ " address = ?, city = ?, state = ?,"
+					+ " telephone = ?, email = ? "
+					+ " where id = ?");
+			
 		}
 		catch (Exception ex) {
 			System.out.println(ex);
+			status = ex.getMessage();
 		}
 	}
 	
 	public void btnView() throws SQLException {
-		String query = "select * from Staff where id = '" + id +"';";
-		Statement stmt = conn.createStatement();
-		ResultSet rs = stmt.executeQuery(query);
-		while (rs.next()) {
-			setLastName(rs.getString("lastName"));
-			setMi(rs.getString("mi"));
-			setFirstName(rs.getString("firstName"));
-			setAddress(rs.getString("address"));
-			setCity(rs.getString("city"));
-			setState(rs.getString("state"));
-			setTelephone(rs.getString("telephone"));
-			setEmail(rs.getString("email"));
-		}
-		stmt.close();
-	}
-	
-	public String storeStaff() {
 		try {
-			pstmt.setString(1, getId());
-			pstmt.setString(2, getLastName());
-			pstmt.setString(3, getMi());
-			pstmt.setString(4, getFirstName());
-			pstmt.setString(5, getAddress());
-			pstmt.setString(6, getCity());
-			pstmt.setString(7, getState());
-			pstmt.setString(8, getTelephone());
-			pstmt.setString(9, getEmail());
-			pstmt.executeUpdate();
+			view.setString(1, getId());
+			ResultSet rs = view.executeQuery();
+			while (rs.next()) {
+				setLastName(rs.getString("lastName"));
+				setMi(rs.getString("mi"));
+				setFirstName(rs.getString("firstName"));
+				setAddress(rs.getString("address"));
+				setCity(rs.getString("city"));
+				setState(rs.getString("state"));
+				setTelephone(rs.getString("telephone"));
+				setEmail(rs.getString("email"));
+			}
+			status = "Staff ID: " + getId() + " loaded successfully.";
+		}
+		catch (SQLException ex) {
+			status = ex.getMessage();
+		}
+	}
+
+	public void btnInsert() {
+		try {
+			insert.setString(1, getId());
+			insert.setString(2, getLastName());
+			insert.setString(3, getMi());
+			insert.setString(4, getFirstName());
+			insert.setString(5, getAddress());
+			insert.setString(6, getCity());
+			insert.setString(7, getState());
+			insert.setString(8, getTelephone());
+			insert.setString(9, getEmail());
+			insert.executeUpdate();
 			status = getFirstName() + " " + getLastName()
 					+ " is now registered in the database.";
 		} 
 		catch (Exception ex) {
 			status = ex.getMessage();
 		}
-		return "StaffStoredStatus";
+	}
+	
+	public void btnUpdate() {
+		try {
+			view.setString(1, getId());
+			ResultSet rs = view.executeQuery();
+			while (rs.next()) {
+				update.setString(1, rs.getString("lastName"));
+				update.setString(2, rs.getString("mi"));
+				update.setString(3, rs.getString("firstName"));
+				update.setString(4, rs.getString("address"));
+				update.setString(5, rs.getString("city"));
+				update.setString(6, rs.getString("state"));
+				update.setString(7, rs.getString("telephone"));
+				update.setString(8, rs.getString("email"));
+			}
+			
+			update.setString(9, getId());
+			
+			if (lastName != null && !lastName.isEmpty())
+				update.setString(1, getLastName());
+			
+			if (mi != null && !mi.isEmpty())
+				update.setString(2, getMi());
+			
+			if (firstName != null && !firstName.isEmpty())
+				update.setString(3, getFirstName());
+			
+			if (address != null && !address.isEmpty())
+				update.setString(4, getAddress());
+			
+			if (city != null && !city.isEmpty())
+				update.setString(5, getCity());
+			
+			if (state != null && !state.isEmpty())
+				update.setString(6, getState());
+			
+			if (telephone != null && !telephone.isEmpty())
+				update.setString(7, getTelephone());
+			
+			if (email != null && !email.isEmpty())
+				update.setString(8, getEmail());
+			
+			System.out.println(update.toString());
+			update.executeUpdate();
+			status = getFirstName() + " " + getLastName()
+					+ " is now updated in the database.";
+		}
+		catch (Exception ex) {
+			status = ex.getMessage();
+		}
+	}
+	
+	public void btnReset() {
+		id = "";
+		firstName = "";
+		mi = "";
+		lastName = "";
+		address = "";
+		city = "";
+		state = "";
+		telephone = "";
+		email = "";
 	}
 	
 	public String getId() {
@@ -164,6 +239,10 @@ public class StaffRegistrationBean implements Serializable{
 
 	public void setEmail(String email) {
 		this.email = email;
+	}
+	
+	public String getStatus() {
+		return status;
 	}
 	
 }
